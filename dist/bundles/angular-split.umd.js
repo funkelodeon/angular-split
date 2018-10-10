57589f12537017572b5180b4250e54abfe16b86d
Nick Funk <nick.funk@sdvi.com>
2018-10-09 17:12:20 -0700

    built

--- dist/bundles/angular-split.umd.js
+++ dist/bundles/angular-split.umd.js
@@ -639,6 +639,7 @@ var SplitComponent = (function () {
      * @return {?}
      */
     function (start, end, areaA, areaB) {
+        console.log("DRAG IS BEING CALLED");
         // ¤ AREAS SIZE PIXEL
         var /** @type {?} */ devicePixelRatio = window.devicePixelRatio || 1;
         var /** @type {?} */ offsetPixel = (this.direction === 'horizontal') ? (start.x - end.x) : (start.y - end.y);
